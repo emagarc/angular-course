5627767f5b3fcd145f9b17c622269dc85c401019
Emanuel Garcia <wemagw@gmail.com>
2023-10-24 19:06:40 -0300

    directivas p1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
 import { SaludoComponent } from './components/saludo/saludo.component';
 
+// Nuestro modulo personalizado que exporta componentes de tipo Lista.
+import { ListsModule } from './modules/lists/lists.module';
+
 @NgModule({
-  declarations: [AppComponent, SaludoComponent],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule], 
+  declarations: [
+    AppComponent, 
+    SaludoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    CommonModule,
+    // importamos nuestro modulo personalizado.
+    ListsModule
+  ], 
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/modules/lists/lista-basica/lista-basica.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-lista-basica',
+  templateUrl: './lista-basica.component.html',
+  styleUrls: ['./lista-basica.component.scss']
+})
+export class ListaBasicaComponent {
+
+}
