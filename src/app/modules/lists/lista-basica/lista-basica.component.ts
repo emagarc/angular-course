d8545ea6702ac0c5302c78c34c7fc0fd4342a9fd
Emanuel Garcia <wemagw@gmail.com>
2023-10-25 16:47:49 -0300

    directivas

--- src/app/modules/lists/lista-basica/lista-basica.component.ts
+++ src/app/modules/lists/lista-basica/lista-basica.component.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// Creamos un tipo base para el ejemplo
+
+export type Producto = {
+  nombre: string;
+  precio: number;
+  descripcion: string;
+}
 
 @Component({
   selector: 'app-lista-basica',
   templateUrl: './lista-basica.component.html',
   styleUrls: ['./lista-basica.component.scss']
 })
-export class ListaBasicaComponent {
+export class ListaBasicaComponent implements OnInit {
+
+  listaElementos: Producto[] = [
+    {
+      nombre: "Leche",
+      precio: 10,
+      descripcion: "Leche entera"
+    },
+    {
+      nombre: "Huevo",
+      precio: 2,
+      descripcion: "Mapple de 12 huevos"
+    },
+    {
+      nombre: "Carne",
+      precio: 25,
+      descripcion: "Carne de cerdo"
+    }
+  ];
+  
+  cargando: boolean = false;
+
+  opcion: number = 0;
+
+  // Define 'elsetemplate' como una propiedad de clase
+  elsetemplate: any; // Puedes ajustar el tipo según tus necesidades
+
+  constructor () {}
+
+  ngOnInit(): void {
+    
+  }
+
+  cambiarCargando() {
+    this.cargando = !this.cargando;
+  }
+
+  escogerOpcion(opcionEscogida: number) {
+    this.opcion = opcionEscogida; // el valor cambia, implica un cambio en los elementos renderizados
+  }
 
 }
